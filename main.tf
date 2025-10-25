2518c99a40dc920b435c31505ec07d8ffda96bd4
ravisankar666 <ravisankarp666@gmail.com>
2025-10-26 01:12:30 +0530

    some message

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 module "vpc" {
-    source = "..\terraform-aws-vpc"
+    source  = "../terraform-aws-vpc"
     #vpc
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     environment = var.environment
     vpc_tags = var.vpc_tags
     #public subnet
-    public_subnet_cidr = var.public_subnet_cidrs
+    public_subnet_cidrs = var.public_subnet_cidrs
 
     #private subnet
 
-    private_subnet_cidr = var.private_subnet_cidrs
+    private_subnet_cidrs = var.private_subnet_cidrs
 
     #database_subnet
-    database_subnet_cidr = var.database_subnet_cidrs
+    database_subnet_cidrs = var.database_subnet_cidrs
 
 }
 # data "aws_availability_zone" "available" {

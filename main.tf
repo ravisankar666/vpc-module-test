ac9961fd3abaed3dc5050cd12a61a387582f17ad
ravisankar666 <ravisankarp666@gmail.com>
2025-10-26 00:54:27 +0530

    some message

--- main.tf
+++ main.tf
@@ -1,8 +1,21 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    source = " ../terraform-aws-vpc"
+    #vpc
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     environment = var.environment
     vpc_tags = var.vpc_tags
-  
+    #public subnet
+    public_subnet_cidr = var.public_subnet_cidrs
+
+    #private subnet
+
+    private_subnet_cidr = var.private_subnet_cidrs
+
+    #database_subnet
+    database_subnet_cidr = var.database_subnet_cidrs
+
 }
+# data "aws_availability_zone" "available" {
+#     state = "available"
+# }
--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "vpc_id" {
     value = module.vpc.vpc_id
   
 }
+# output "azs" {
+#     value = data.aws_availability_zone.available.name
+  
+# }
